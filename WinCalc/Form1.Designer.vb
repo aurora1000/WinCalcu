ab2bd82567f8d46272fa963ce7a8530261d76f93
Rem Mongcal <52030682+aurora1000@users.noreply.github.com>
2020-01-17 19:41:24 +0800

    0-9 button functions

--- WinCalc/Form1.Designer.vb
+++ WinCalc/Form1.Designer.vb
@@ -25,7 +25,6 @@ Partial Class Calculator
         Me.Button1 = New System.Windows.Forms.Button()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Button2 = New System.Windows.Forms.Button()
-        Me.Label2 = New System.Windows.Forms.Label()
         Me.Button3 = New System.Windows.Forms.Button()
         Me.Button4 = New System.Windows.Forms.Button()
         Me.Button5 = New System.Windows.Forms.Button()
@@ -70,6 +69,7 @@ Partial Class Calculator
         Me.Button44 = New System.Windows.Forms.Button()
         Me.Button45 = New System.Windows.Forms.Button()
         Me.Button46 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.SuspendLayout()
         '
         'Button1
@@ -110,19 +110,6 @@ Partial Class Calculator
         Me.Button2.Size = New System.Drawing.Size(24, 23)
         Me.Button2.TabIndex = 2
         Me.Button2.UseVisualStyleBackColor = False
-        '
-        'Label2
-        '
-        Me.Label2.BackColor = System.Drawing.SystemColors.ButtonFace
-        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 35.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.ForeColor = System.Drawing.Color.Black
-        Me.Label2.Location = New System.Drawing.Point(8, 63)
-        Me.Label2.Name = "Label2"
-        Me.Label2.RightToLeft = System.Windows.Forms.RightToLeft.No
-        Me.Label2.Size = New System.Drawing.Size(408, 75)
-        Me.Label2.TabIndex = 3
-        Me.Label2.Text = "0"
-        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight
         '
         'Button3
         '
@@ -743,12 +730,28 @@ Partial Class Calculator
         Me.Button46.Text = "DEG"
         Me.Button46.UseVisualStyleBackColor = False
         '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBox1.Enabled = False
+        Me.TextBox1.Font = New System.Drawing.Font("Segoe UI Semibold", 48.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.ForeColor = System.Drawing.Color.Black
+        Me.TextBox1.Location = New System.Drawing.Point(8, 62)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.TextBox1.Size = New System.Drawing.Size(408, 86)
+        Me.TextBox1.TabIndex = 48
+        Me.TextBox1.Text = "0"
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
         'Calculator
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.SystemColors.ButtonFace
         Me.ClientSize = New System.Drawing.Size(424, 652)
+        Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Button44)
         Me.Controls.Add(Me.Button45)
         Me.Controls.Add(Me.Button46)
@@ -793,7 +796,6 @@ Partial Class Calculator
         Me.Controls.Add(Me.Button5)
         Me.Controls.Add(Me.Button4)
         Me.Controls.Add(Me.Button3)
-        Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.Button1)
@@ -812,7 +814,6 @@ Partial Class Calculator
     Friend WithEvents Button1 As Button
     Friend WithEvents Label1 As Label
     Friend WithEvents Button2 As Button
-    Friend WithEvents Label2 As Label
     Friend WithEvents Button3 As Button
     Friend WithEvents Button4 As Button
     Friend WithEvents Button5 As Button
@@ -857,4 +858,5 @@ Partial Class Calculator
     Friend WithEvents Button44 As Button
     Friend WithEvents Button45 As Button
     Friend WithEvents Button46 As Button
+    Friend WithEvents TextBox1 As TextBox
 End Class
--- WinCalc/Form1.vb
+++ WinCalc/Form1.vb
@@ -4,43 +4,43 @@
     End Sub
 
     Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
-
+        TextBox1.Text = TextBox1.Text & "0"
     End Sub
 
     Private Sub Button11_Click(sender As Object, e As EventArgs) Handles Button11.Click
-
+        TextBox1.Text = TextBox1.Text & "1"
     End Sub
 
     Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
-
+        TextBox1.Text = TextBox1.Text & "2"
     End Sub
 
     Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
-
+        TextBox1.Text = TextBox1.Text & "3"
     End Sub
 
     Private Sub Button14_Click(sender As Object, e As EventArgs) Handles Button14.Click
-
+        TextBox1.Text = TextBox1.Text & "6"
     End Sub
 
     Private Sub Button15_Click(sender As Object, e As EventArgs) Handles Button15.Click
-
+        TextBox1.Text = TextBox1.Text & "5"
     End Sub
 
     Private Sub Button16_Click(sender As Object, e As EventArgs) Handles Button16.Click
-
+        TextBox1.Text = TextBox1.Text & "4"
     End Sub
 
     Private Sub Button21_Click(sender As Object, e As EventArgs) Handles Button21.Click
-
+        TextBox1.Text = TextBox1.Text & "7"
     End Sub
 
     Private Sub Button20_Click(sender As Object, e As EventArgs) Handles Button20.Click
-
+        TextBox1.Text = TextBox1.Text & "8"
     End Sub
 
     Private Sub Button19_Click(sender As Object, e As EventArgs) Handles Button19.Click
-
+        TextBox1.Text = TextBox1.Text & "9"
     End Sub
 
     Private Sub Button13_Click(sender As Object, e As EventArgs) Handles Button13.Click
@@ -53,5 +53,10 @@
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
 
+    End Sub
+
+    Private Sub Button25_Click(sender As Object, e As EventArgs) Handles Button25.Click
+        TextBox1.Clear()
+        TextBox1.Text = TextBox1.Text & "0"
     End Sub
 End Class
